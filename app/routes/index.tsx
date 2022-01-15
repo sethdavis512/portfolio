2af02a2c845733d47a1a0aafcfcb334a3442d927
Seth Davis <sethdavis512@gmail.com>
2022-01-15 11:04:46 -0600

    first commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Index() {
+    return (
+        <div className="">
+            <h1 className="text-2xl">Portfolio</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore fugiat soluta laudantium autem, sunt, optio atque, perferendis fuga alias facere suscipit accusantium molestias! Molestiae, esse vero ipsa eos fugiat veritatis.
+            </p>
+            <div className="card">
+                Stuff!
+            </div>
+        </div>
+    );
+}
