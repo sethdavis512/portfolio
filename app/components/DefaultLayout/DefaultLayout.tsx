4cd298693883558fa2621789b6f2a1fd3afda3de
Seth Davis <sethdavis512@gmail.com>
2022-05-02 21:32:12 -0500

    Update styles

--- app/components/DefaultLayout/DefaultLayout.tsx
+++ app/components/DefaultLayout/DefaultLayout.tsx
@@ -8,7 +8,7 @@ const DefaultLayout = ({
     main: ReactNode;
 }) => {
     return (
-        <div className="w-full h-full px-6 py-4 flex flex-col items-center justify-center">
+        <div className="w-full h-full px-6 py-4 flex flex-col items-center pt-16 sm:pt-0 sm:justify-center">
             <header className="">{header}</header>
             <main className="w-full">{main}</main>
         </div>
