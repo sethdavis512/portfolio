ef3fc9062f81545b9ed04c5ffb3e6cbd51da55e1
Seth Davis <sethdavis512@gmail.com>
2024-07-03 16:51:26 -0500

    Add post hog

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -5,14 +5,27 @@
  */
 
 import { RemixBrowser } from '@remix-run/react';
-import { startTransition, StrictMode } from 'react';
+import { startTransition, StrictMode, useEffect } from 'react';
 import { hydrateRoot } from 'react-dom/client';
+import posthog from 'posthog-js';
+
+function PosthogInit() {
+    useEffect(() => {
+        posthog.init('phc_NIVP9tJ3sH5xgM3DFKV4ADJFOJb14DCG6KBMzGiHPeC', {
+            api_host: 'https://us.i.posthog.com',
+            person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+        });
+    }, []);
+
+    return null;
+}
 
 startTransition(() => {
     hydrateRoot(
         document,
         <StrictMode>
             <RemixBrowser />
+            <PosthogInit />
         </StrictMode>
     );
 });
