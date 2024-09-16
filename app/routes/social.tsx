68cb934a0d9fe4b02090affccc73c47d7593915f
Seth Davis <sethdavis512@gmail.com>
2024-09-16 10:27:37 -0500

    Add social route

---
+++ app/routes/social.tsx
@@ -0,0 +1,7 @@
+import { redirect } from '@remix-run/node';
+
+export async function loader() {
+    return redirect(
+        'https://facetime.apple.com/join#v=1&p=hfAc33FREe+8FwYsLQZ+aQ&k=HijbaHltbQZTVHfedYU9fVd69AiZIO9PfK4Z3LMBTeI'
+    );
+}
