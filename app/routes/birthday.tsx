b927c151b1220f0666e5133eb412d9e1dab15ae3
Seth Davis <sethdavis512@gmail.com>
2024-09-23 21:36:14 -0500

    Update text

--- app/routes/birthday.tsx
+++ app/routes/birthday.tsx
@@ -13,11 +13,11 @@ export default function BirthdayRoute() {
             <div className="flex flex-col-reverse gap-4 px-2 py-4 md:flex-row md:pt-12">
                 <div className="flex-1">
                     <h1 className="mb-8 text-4xl font-bold">
-                        Seth is turning {age}
+                        Seth is turning {age} 🥳
                     </h1>
                     <div className="space-y-4">
                         <h2 className="text-3xl font-bold">Details</h2>
-                        <h3 className="text-xl">{`🔥 We're having a bonfire hangout 🍻`}</h3>
+                        <h3 className="text-xl">{`🔥 We're celebrating with a bonfire hangout 🍻`}</h3>
                         <p>
                             <strong>Date:</strong> Friday, October 11
                         </p>
@@ -30,30 +30,17 @@ export default function BirthdayRoute() {
                                 className="text-green-500 underline"
                                 href="https://maps.app.goo.gl/WaqhCa2LSKL6geaa9"
                             >
-                                1109 Oakwood Drive Leander TX 78641
+                                1109 Oakwood Drive Leander, TX 78641
                             </a>
-                        </p>
-                        <p>
-                            <details className="inline-block rounded-lg border-2 border-zinc-300 px-4 py-2">
-                                <summary>Show me a map</summary>
-                                <iframe
-                                    title="Davis Home"
-                                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3436.128247280264!2d-97.85885052364611!3d30.545680474675088!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x865b2c1673c26ad9%3A0x4fe6f37e64efdc1e!2s1109%20Oakwood%20Dr%2C%20Leander%2C%20TX%2078641!5e0!3m2!1sen!2sus!4v1727112145060!5m2!1sen!2sus"
-                                    width="600"
-                                    height="450"
-                                    className="border-none"
-                                    loading="lazy"
-                                    referrerPolicy="no-referrer-when-downgrade"
-                                ></iframe>
-                            </details>
                         </p>
                         <p>
                             <strong>Who:</strong> You. Significant others and
                             kids are welcome.
                         </p>
                         <p>
-                            <strong>What:</strong> Come hangout & enjoy some
-                            smores. Eat before you show up. Bring your own beer.
+                            <strong>What:</strong> Come hangout, sit by the fire
+                            & enjoy some tasty food and smores. Beer will be
+                            provided.
                         </p>
                     </div>
                 </div>
