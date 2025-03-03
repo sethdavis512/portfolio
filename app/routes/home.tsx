1893286e60591d04125b2d8ca95ac5e2ba5ffa97
Seth Davis <sethdavis512@gmail.com>
2025-03-03 16:23:32 -0600

    Remove route. Add images back in.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,12 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: `Seth Davis' Portfolio` },
+    { name: "description", content: "Welcome to Seth Davis' Portfolio" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <>Dude. Nice.</>;
 }
