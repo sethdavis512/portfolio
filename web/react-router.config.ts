44369b16275bb006f189d08f29a4028cd3da814a
Seth Davis <sethdavis512@gmail.com>
2025-04-20 12:58:04 -0500

    Remove blog from prerender

--- web/react-router.config.ts
+++ web/react-router.config.ts
@@ -3,6 +3,6 @@ import type { Config } from '@react-router/dev/config';
 export default {
     ssr: true,
     async prerender() {
-        return [`/`, `/about`, `/resume`, `/setup`, `/truck`, `/blog`];
+        return [`/`, `/about`, `/resume`, `/setup`, `/truck`];
     }
 } satisfies Config;
