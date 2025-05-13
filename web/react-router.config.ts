0cf477326fe6f4e57841d1379ea22ef4c329a907
Seth Davis <sethdavis512@gmail.com>
2025-05-13 16:58:29 -0500

    Prerender project pages

--- web/react-router.config.ts
+++ web/react-router.config.ts
@@ -3,6 +3,14 @@ import type { Config } from '@react-router/dev/config';
 export default {
     ssr: true,
     async prerender() {
-        return [`/`, `/about`, `/resume`, `/setup`, `/truck`];
+        return [
+            `/`,
+            `/about`,
+            `/resume`,
+            `/setup`,
+            `/truck`,
+            `/projects/tws-cms`,
+            `/projects/tws-starter`
+        ];
     }
 } satisfies Config;
